fb41da045bab8c5b36202286ff41e8bd74b17f0e
Stevie Rossetti <rossetti.stephen@gmail.com>
2023-11-23 01:20:23 -0600

    Added comments for reviewer to udnerstand process and for me at a further date

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Define all variables. getElementbyID from HTML and utilize other variables to refresh every time a function is ended//
 let timeEl = document.getElementById("timer");
 let startBtn = document.getElementById("startBtn");
 let nextBtn = document.getElementById("nextBtn");
@@ -20,11 +21,13 @@ let nameFormat = document.getElementById("nameFormat");
 let leaderBoard = document.getElementById("leaderboard");
 let names = [];
 
+//Hide the homepage after quiz starts//
 function hideHomePage () {
   image.style.display = "none";
   startBtn.style.display = "none";
 }
 
+//Start the game. Starts at 100 seconds, shuffles the questions, shows the question, and hides homepage//
 function startGame() {
   timeLeft = 101;
   shuffledQuestions = questionArray.sort(() => Math.random() - 0.5);
@@ -34,6 +37,7 @@ function startGame() {
   countDown ();
 }
 
+//Countdown for game/highscore. Normal time intervarl, but added an if/else statement so if it hits 0 there will be an alert and end the quiz. Also added clear interval to reset the timer//
 function countDown() {
         timeInterval = setInterval(() => {
         timeLeft--;
@@ -50,12 +54,15 @@ function countDown() {
     }, 1000);
 }
 
+//Physically shows the next question when quiz starts, or next button is clocked, by adding the Container. Also hides previous question//
 function showQuestion () {
   questionContainerEl.style.display = "block";
   hidePreviousQuestion();
   inputQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
+//This function decides what is inputted into the question (i.e., grabs the answers and respective questions from array and places them within the pre-define HTML)//
+//Function also includes an appended "button" that allows us to visually see if an answer was right or wrong based on colors in css (red/green)//
 function inputQuestion (question) {
   questionEl.innerText = question.question;
   question.answers.forEach(answer => {
@@ -70,6 +77,8 @@ function inputQuestion (question) {
   })
 }
 
+//Hides previous questions as well next/back buttons that display after questions are answered//
+//This also removes the template answer data in the HTML (i.e., "Answer 1, Answer 2...". I kept the template for button functionally but can remove it visually)
 function hidePreviousQuestion () {
   backBtn.style.display = "none";
   nextBtn.style.display = "none";
@@ -79,6 +88,8 @@ function hidePreviousQuestion () {
   }
 }
 
+//Appended answer buttons can be clicked here by clicking the "event target" (i.e., where I'm clicking on screen). This applies the class list in css for wrong/right answers//
+//Also gives the next button its text//
 function selectAnswer (e) {
   let answerClicked = e.target;
   let answerCorrect = answerClicked.dataset.correct === "true";
@@ -92,6 +103,7 @@ function selectAnswer (e) {
   nextBtn.style.display = "block";
 }
 
+//Created this function so a wrong answer that minuses 15 seconds doesn't make the time for in the negatives and skip 0. It auto goes to "0". For some reason setting the variable to 1 makes it go to 0//
 function timerMinus () {
   if (timeLeft >= 15 ) {
     timeLeft -= 15;
@@ -100,6 +112,7 @@ function timerMinus () {
   }
 }
 
+//Function to go to the next question, but if all are answered in the array it will go to the score page and stop the time//
 function nextQuestion () {
   currentQuestionIndex++;
   if (currentQuestionIndex < questionArray.length) {
@@ -109,6 +122,7 @@ function nextQuestion () {
   }
 }
 
+//Highscore page hides previous questions and determines a message based on time left. Added timeLeft variable to reflect the score (had to minus one to make them match)// 
 function highScore () {
   hidePreviousQuestion ();
   if (timeLeft <= 0) {
@@ -120,6 +134,7 @@ function highScore () {
   backBtn.style.display = "block";
 }
 
+//Next button clicks if there are still remaining questions. Originally had this function serve as a restart and next question but the code wasn't working. Created a new replay button instead//
 nextBtn.addEventListener("click", ()=>{
   if(currentQuestionIndex < questionArray.length) {
   nextQuestion();
@@ -128,6 +143,7 @@ nextBtn.addEventListener("click", ()=>{
 
 startBtn.addEventListener("click", startGame);
 
+//Created an array for questions and answers (together). This not only designates true and false answers but also allows questions to stay with answers and display over the HTML template//
 let questionArray = [
   { question: "How can a datatype be declared to be a constant type?",
     answers: [
@@ -166,6 +182,9 @@ let questionArray = [
   ] },
 ];
 
+//Everything below here is local storage code to save high scores and initials//
+
+//Essentially a loop to keep appending new list items (i.e., high scores) everytime you play and hit submit. Similar to shopping list activity//
 function renderNames () {
   nameList.innerHTML = "";
   for (let j = 0; j < names.length; j++) {
@@ -178,6 +197,7 @@ function renderNames () {
   }
 }
 
+//A string to parse as JSON//
 function init() {
   let storedNames = JSON.parse(localStorage.getItem("names"));
   if (storedNames !== null) {
@@ -186,14 +206,17 @@ function init() {
   renderNames ();
 }
 
+//Stringifies data in local storage//
 function storeNames () {
   localStorage.setItem("names", JSON.stringify(names));
 }
 
+//Hides the Initials input after submitting so you can't save you score twice//
 function hideInputBox () {
   nameForm.style.display = "none";
 }
 
+//Runs the function by submitting initials. I added timeLeft here so it saves the timer locally and doesn't restart the amount every new game//
 nameForm.addEventListener("submit", function(event) {
   event.preventDefault();
   let nameText = nameTextEl.value + " - " + timeLeft + " pts";
@@ -211,6 +234,7 @@ nameForm.addEventListener("submit", function(event) {
 
 highScoreBtn.addEventListener("click",getScores);
 
+//Function for when HighScore button in navbar is clicked. Provides alternative to going through the whole quiz. Displays necessary areas.//
 function getScores () {
   clearInterval(timeInterval);
   hideHomePage ();
@@ -220,6 +244,7 @@ function getScores () {
   backBtn.style.display = "block";
 }
 
+//Late addition for efficiency to include all elements that need to be displayed at one time for the highscore pages//
 function formPage () {
   nameContainer.style.display = "block";
   nameForm.style.display = "block";
@@ -227,5 +252,6 @@ function formPage () {
   nameTextEl.style.display = "block";
 }
 
+//Back buttoon because next button wouldn't work to restart game correctly//
 backBtn.addEventListener("click",startGame);
 
